4cb5bf6af9f3c90d3c45b6fd9b39ecfe06bdebf8
tkddnr0524 <tkddnr0110@naver.com>
2024-03-25 18:38:11 +0900

    [Feat] Created Interaction

---
+++ Poteking/Assets/Script/InteractiveObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+
+    private GameObject interactiveTarget;
+
+}
