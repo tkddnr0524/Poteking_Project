0366da6ff5122879c5928a1edaec8a3783afd5e2
KKJ1000 <kwangjoon1733@naver.com>
2024-03-25 18:37:29 +0900

    [Feat] playerMove, SceneChange
    플레이어 이동, 씬 이동 구현

---
+++ Poteking/Assets/Scripts/GameScript/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float maxSpeed;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+
+}
