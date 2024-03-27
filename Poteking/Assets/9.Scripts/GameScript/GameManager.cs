4322480f5fdc5309d71a7f4ec109892bd4b67dcd
KKJ1000 <kwangjoon1733@naver.com>
2024-03-27 22:09:43 +0900

    Revert "[fix] 플레이어 이동 수정 및 씬 추가"
    This reverts commit 3fa4614ce224ead9ac115d897e4f94dad1a63250.

--- Poteking/Assets/9.Scripts/GameScript/GameManager.cs
+++ Poteking/Assets/9.Scripts/GameScript/GameManager.cs
@@ -8,19 +8,14 @@ public class GameManager : MonoBehaviour
     public GameObject notification1; // 첫번째 알림창 이미지 넣을 공간
     public GameObject notification2; // 두번째 알림창 이미지 넣을 공간
 
-    public GameObject chatObject; //Chat = 대화창 오브젝트가 들어갈 공간
     void Start()
     {
-        //게임 시작되면 Chat 오브젝트를 비활성화
-        chatObject.SetActive(false);
-
-
         //게임이 시작되면 알림창을 비활성화 상태로 설정해놓음
         notification1.SetActive(false);
         notification2.SetActive(false);
 
         //각각의 알림창에 대해 활성화 딜레이 설정
-        Invoke("ActiveNotification1", 2f);
+        Invoke("ActiveNotification1", 3f);
         Invoke("ActiveNotification2", 6f);
         
     }
@@ -33,21 +28,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // 알림창을 클릭 했을 때 
-    public void OnNotificationClicked()
-    {
-        if(chatObject != null)
-        {
-            //Chat 오브젝트 활성화
-            chatObject.SetActive(true);
-
-            // Chat 오브젝트와 모든 자식 오브젝트(대화 말풍선과 텍스트)도 함께 활성화
-            foreach (Transform child in chatObject.transform) 
-            {
-                child.gameObject.SetActive(true);
-            }
-        }
-    }
     void ActiveNotification2()
     {
         //두번째 알림창 이미지를 활성화 
--- Poteking/Assets/9.Scripts/GameScript/PlayerController.cs
+++ Poteking/Assets/9.Scripts/GameScript/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,49 +8,18 @@ public class PlayerController : MonoBehaviour
 {
     ObjectData currentObject;
     public TalkAction talkAction;
-
     Rigidbody2D rigid;
-    public float moveSpeed = 5f; // 플레이어의 이동 속도
-    private float moveInput;
+    public float maxSpeed;
 
-    void Start()
+    private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();    
+        rigid = GetComponent<Rigidbody2D>();
+        
     }
 
-    void Update()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
+    
 
-        /*if (currentObject != null && Input.GetKeyDown(KeyCode.E))
-        {
-            talkAction.Action(currentObject.gameObject);
-        }*/
-    }
-
-    private void FixedUpdate()
-    {
-        //이동 
-        rigid.velocity = new Vector2(moveInput * moveSpeed, rigid.velocity.y);
-
-        // 플레이어 이동 방향
-        if(moveInput > 0) //오른쪽으로 갈때
-        {
-            transform.localScale = new Vector3(1,1,1); 
-        }
-        else if (moveInput < 0) //왼쪽으로 갈때
-        {
-            transform.localScale = new Vector3(-1, 1, 1); 
-        }
-
-
-
-        /*if (!talkAction.isAction)
-        {
-        }*/
-    }
-
-   /* void OnTriggerEnter2D(Collider2D scanObject)
+    void OnTriggerEnter2D(Collider2D scanObject)
     {
         ObjectData isObject = scanObject.GetComponent<ObjectData>();
         if(isObject != null)
@@ -59,6 +27,8 @@ public class PlayerController : MonoBehaviour
             currentObject = isObject;
             Debug.Log(scanObject.name + "와 충돌하였습니다");
         }
+
+        
     }
 
     private void OnTriggerExit2D(Collider2D scanObject)
@@ -69,11 +39,35 @@ public class PlayerController : MonoBehaviour
             currentObject = null;
             Debug.Log(scanObject.name + "와 충돌이 끝났습니다");
         }
-    }*/
+    }
+    private void FixedUpdate()
+    {
 
-    
+        if (!talkAction.isAction) 
+        {
+            float h = Input.GetAxisRaw("Horizontal");
 
-    
+
+
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+
+
+            if (rigid.velocity.x > maxSpeed)
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            else if (rigid.velocity.x < maxSpeed * (-1))
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+    }
+
+    void Update()
+    {
+        if(currentObject != null && Input.GetKeyDown(KeyCode.E))
+        {
+            talkAction.Action(currentObject.gameObject); 
+
+        }
+    }
 
 
     
