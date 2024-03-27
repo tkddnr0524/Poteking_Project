3fa4614ce224ead9ac115d897e4f94dad1a63250
KKJ1000 <kwangjoon1733@naver.com>
2024-03-27 22:08:29 +0900

    [fix] 플레이어 이동 수정 및 씬 추가

--- Poteking/Assets/9.Scripts/GameScript/PlayerController.cs
+++ Poteking/Assets/9.Scripts/GameScript/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,18 +9,49 @@ public class PlayerController : MonoBehaviour
 {
     ObjectData currentObject;
     public TalkAction talkAction;
-    Rigidbody2D rigid;
-    public float maxSpeed;
 
-    private void Awake()
+    Rigidbody2D rigid;
+    public float moveSpeed = 5f; // 플레이어의 이동 속도
+    private float moveInput;
+
+    void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        
+        rigid = GetComponent<Rigidbody2D>();    
     }
 
-    
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
 
-    void OnTriggerEnter2D(Collider2D scanObject)
+        /*if (currentObject != null && Input.GetKeyDown(KeyCode.E))
+        {
+            talkAction.Action(currentObject.gameObject);
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        //이동 
+        rigid.velocity = new Vector2(moveInput * moveSpeed, rigid.velocity.y);
+
+        // 플레이어 이동 방향
+        if(moveInput > 0) //오른쪽으로 갈때
+        {
+            transform.localScale = new Vector3(1,1,1); 
+        }
+        else if (moveInput < 0) //왼쪽으로 갈때
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
+
+
+
+        /*if (!talkAction.isAction)
+        {
+        }*/
+    }
+
+   /* void OnTriggerEnter2D(Collider2D scanObject)
     {
         ObjectData isObject = scanObject.GetComponent<ObjectData>();
         if(isObject != null)
@@ -27,8 +59,6 @@ public class PlayerController : MonoBehaviour
             currentObject = isObject;
             Debug.Log(scanObject.name + "와 충돌하였습니다");
         }
-
-        
     }
 
     private void OnTriggerExit2D(Collider2D scanObject)
@@ -39,35 +69,11 @@ public class PlayerController : MonoBehaviour
             currentObject = null;
             Debug.Log(scanObject.name + "와 충돌이 끝났습니다");
         }
-    }
-    private void FixedUpdate()
-    {
+    }*/
 
-        if (!talkAction.isAction) 
-        {
-            float h = Input.GetAxisRaw("Horizontal");
+    
 
-
-
-            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-
-
-            if (rigid.velocity.x > maxSpeed)
-                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-            else if (rigid.velocity.x < maxSpeed * (-1))
-                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-        }
-    }
-
-    void Update()
-    {
-        if(currentObject != null && Input.GetKeyDown(KeyCode.E))
-        {
-            talkAction.Action(currentObject.gameObject); 
-
-        }
-    }
+    
 
 
     
