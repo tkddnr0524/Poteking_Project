9597f3473f79867acd3d17df029b484bd33363d7
KKJ1000 <kwangjoon1733@naver.com>
2024-04-08 18:37:09 +0900

    [Feat] 대화 내용 업데이트 기능 예제

--- Poteking/Assets/Scripts/GameScript/InteractiveAction.cs
+++ Poteking/Assets/Scripts/GameScript/InteractiveAction.cs
@@ -2,43 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InteractiveAction : MonoBehaviour
 {
-    //ObjectData currentObject;
-    //public TalkAction talkAction;
-
-    private void Awake()
-    {
-        /* talkAction = GetComponent<TalkAction>();*/
-        //talkAction = FindObjectOfType<TalkAction>();
-    }
-
-
+    public Image dialogueImg; //대화창배경 이미지 넣을 공간
+   
     void OnTriggerEnter2D(Collider2D scanObject)
     {
-        //ObjectData isObject = scanObject.GetComponent<ObjectData>();
-        //if (isObject != null)
-        //{
-        //    if (!talkAction.isAction)
-        //    {
-
-        //        currentObject = isObject;
-        //        Debug.Log(scanObject.name + "와 충돌하였습니다");
-        //    }
-        //}
-
+       
 
     }
 
     private void OnTriggerExit2D(Collider2D scanObject)
     {
-        //ObjectData isObject = scanObject.GetComponent<ObjectData>();
-        //if (isObject != null && isObject == currentObject)
-        //{
-        //    currentObject = null;
-        //    Debug.Log(scanObject.name + "와 충돌이 끝났습니다");
-        //}
+        
     }
     // Start is called before the first frame update
     void Start()
@@ -49,13 +27,6 @@ public class InteractiveAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (currentObject != null && Input.GetKeyDown(KeyCode.F))
-        //{
-        //    talkAction.Action(currentObject.gameObject);
-        //    {
-        //        Debug.Log("액션실행");
-        //    }
-        //    Debug.Log("상호작용");
-        //}
+       
     }
 }
