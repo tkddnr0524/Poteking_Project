479af06f43f56e665374aa83744e2efe572a63cb
KKJ1000 <kwangjoon1733@naver.com>
2024-04-12 23:13:04 +0900

    [Feat] 맵에 NPC 오브젝트 및 카메라 스크립트 수정
    프로토타이핑에 필요한 NPC배치와 카메라스크립트 간단히 수정, 카메라가 맵을 벗어나지않도록 X좌표 제한을 두는 것이 아닌 맵 이미지를 넓혀 벗어나보이지 않도록 할 예정, 페이드 인/ 페이드 아웃으로 포탈을 통해 맵 변환 간 불필요한 공간이 보이는 것을 방지 할 예정

---
+++ Poteking/Assets/Scripts/GameScript/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player; //플레이어 위치
+    public Vector3 offset; //플레이어와 카메라 사이 거리
+
+    private void Update()
+    {
+        Vector3 playerPosition = player.position + offset;
+        transform.position = new Vector3(playerPosition.x, playerPosition.y, transform.position.z);
+    }
+}
