d4060dc80ab44776b54cd55fd0eed1e6e7c8d5e9
KKJ1000 <kwangjoon1733@naver.com>
2024-04-17 18:50:11 +0900

    [Feat] 맵 배치, SplashScene FadeOut, MainMenu FadeIn

---
+++ Poteking/Assets/Scripts/UIScript/MainMenuSceneManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuSceneManager : MonoBehaviour
+{
+    Image fadeImage;
+
+    private void Awake()
+    {
+        fadeImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn(3.0f));
+    }
+
+    private IEnumerator FadeIn(float duration)
+    {
+        fadeImage.enabled = true;
+
+        Color color = fadeImage.color;
+        float elapsedTime = 0f;
+        while (fadeImage.color.a > 0f) //알파 값이 0이 될때까지 반복
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = 1f - (elapsedTime / duration);
+            fadeImage.color = color;
+            yield return null; //한 프레임동안 대기한다.
+        }
+
+        color.a = 0f;
+        fadeImage.color = color;
+        fadeImage.enabled = false;
+    }
+
+}
---
+++ Poteking/Assets/Scripts/UIScript/SplashSceneManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashSceneManager : MonoBehaviour
+{
+    Image fadeImage;
+
+    private void Awake()
+    {
+        fadeImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeOut(4.0f));
+    }
+
+    private IEnumerator FadeOut(float duration)
+    {
+        fadeImage.enabled = true;
+
+        Color color = fadeImage.color;
+        float elapsedTime = 0f;
+        while (fadeImage.color.a < 1f) //알파 값이 0이 될때까지 반복
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = elapsedTime / duration;
+            fadeImage.color = color;
+            yield return null; //한 프레임동안 대기한다.
+        }
+
+        color.a = 1f;
+        fadeImage.color = color;
+        SceneManager.LoadScene("01.MainMenu");
+    }
+
+}
